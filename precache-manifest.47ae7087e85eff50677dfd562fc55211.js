703d3c1174d8ce5d0ddea78dc2fd47bcb45b04b0
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 22:12:30 -0400

    Updates

---
+++ precache-manifest.47ae7087e85eff50677dfd562fc55211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c3c0e8e4f1864599cf2ff8301a54a9",
+    "url": "/movieDatabase/index.html"
+  },
+  {
+    "revision": "91c7f7cb9346a4fea2ae",
+    "url": "/movieDatabase/static/css/main.289c1fa7.chunk.css"
+  },
+  {
+    "revision": "9aff709ffce6dd2dd0a7",
+    "url": "/movieDatabase/static/js/2.5b374e73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieDatabase/static/js/2.5b374e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c7f7cb9346a4fea2ae",
+    "url": "/movieDatabase/static/js/main.ec6ae429.chunk.js"
+  },
+  {
+    "revision": "1693d95e32eb02b4a7e5",
+    "url": "/movieDatabase/static/js/runtime-main.417f4aae.js"
+  }
+]);
